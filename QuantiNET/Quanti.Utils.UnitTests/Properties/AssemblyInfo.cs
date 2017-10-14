8235de977caf0af5c51edee0b830bb728851118e
hudecekd <dusan.hudecek@quanti.cz>
2017-10-14 14:38:50 +0200

    version set to 0.0.1

--- QuantiNET/Quanti.Utils.UnitTests/Properties/AssemblyInfo.cs
+++ QuantiNET/Quanti.Utils.UnitTests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("43650b97-0a5c-4f16-b72a-ce555e0a429a")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
