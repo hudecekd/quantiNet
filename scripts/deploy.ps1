e951e9bea530e203136ecfb1792fa7716256a859
hudecekd <dusan.hudecek@quanti.cz>
2017-10-29 12:50:57 +0100

    check deploy errors.

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -11,8 +11,24 @@ cd ..
 
 # TODO: is there a better way to locate nupkg files? Somehow specify exact path without '*'?
 Invoke-Expression "$nuget push -Source $nexusUrl -ApiKey $apiKey  Quanti.Utils.*.nupkg"
+if ($LastExitCode -ne -0)
+{
+  echo "Deploy failed."
+  exit $LastExitCode
+}
+
 Invoke-Expression "$nuget push -Source $nexusUrl -ApiKey $apiKey  Quanti.WPF.Utils.*.nupkg"
+if ($LastExitCode -ne -0)
+{
+  echo "Deploy failed."
+  exit $LastExitCode
+}
 
 # When defualt bin folder is not changed msbuild adds netstandard folder by default. When it is changed we need to delete it so it behaves correctly
 # and nuget packages is in "$configuration" folder.
 Invoke-Expression "$nuget push -Source $nexusUrl -ApiKey $apiKey QuantiNET\Quanti.Utils\bin.S20\$configuration\Quanti.Utils.S20.*.nupkg"
+if ($LastExitCode -ne -0)
+{
+  echo "Deploy failed."
+  exit $LastExitCode
+}
