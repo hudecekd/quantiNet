357d5f58f1f1b40b7733a319b494fe58ae7d0edc
hudecekd <dusan.hudecek@quanti.cz>
2017-10-31 22:14:46 +0100

    nuget pack output

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,6 +2,7 @@
 
 # nuget properties
 [string] $licenseUrl = "http://www.quanti.cz"
+[string] $outputDirectory = "\output\$configuration";
 
 # go to root folder
 $currentPathBackup = (Get-Location).Path
@@ -26,8 +27,19 @@ Try
     echo "Build of QuantiNET failed."
     exit $LastExitCode
   }
-  Invoke-Expression "$nuget pack QuantiNET\Quanti.Utils\Quanti.Utils.csproj -Prop Configuration=$configuration -Properties licenseUrl=$licenseUrl"
-  Invoke-Expression "$nuget pack QuantiNET\Quanti.WPF.Utils\Quanti.WPF.Utils.csproj -Prop Configuration=$configuration -Properties licenseUrl=$licenseUrl"
+
+  # create output directory where nuget packages will be saved only when it does not exists.
+  if (!(Test-Path -Path $outputDirectory))
+  {
+    New-Item -ItemType directory $outputDirectory
+  }
+
+  # msbuild of .NET Standard uses output directory of project.
+  # Do not know whether we can set path only for nuget package so we need to copy it from build folder
+  Copy-Item QuantiNET\Quanti.Utils\bin.S20\$configuration\Quanti.Utils.S20.*.nupkg $outputDirectory
+
+  Invoke-Expression "$nuget pack QuantiNET\Quanti.Utils\Quanti.Utils.csproj -OutputDirectory $outputDirectory -Prop Configuration=$configuration -Properties licenseUrl=$licenseUrl"
+  Invoke-Expression "$nuget pack QuantiNET\Quanti.WPF.Utils\Quanti.WPF.Utils.csproj -OutputDirectory $outputDirectory -Prop Configuration=$configuration -Properties licenseUrl=$licenseUrl"
 }
 Catch
 {
